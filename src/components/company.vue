7c7746d7b5c50cd31d07dee2094f6897829a8baa
subinsad <subin_1118@naver.com>
2023-06-06 22:41:34 +0900

    chart.js 글씨추가

--- src/components/company.vue
+++ src/components/company.vue
@@ -1,21 +1,32 @@
 <template>
-  <LayoutLong title="업체별 출역현황" class="layout">
-    <BarChart class="layout__chart" />
+    <LayoutLong title="업체별 출역현황" class="layout">
+        <div class="layout__wrap">
+            <BarChart class="layout__chart" />
 
-    <div class="layout__total">
-      <span> 45명</span>
-    </div>
+            <div class="layout__wrap__total">
+                <span> 45명</span>
+            </div>
+            <ul class="list">
+                <li class="list__text-box">
+                    <span class="list__text-box__box"></span>
+                    <span class="list__text-box__text">{{ name }}</span>
+                    <span>{{ number }}명</span>
+                </li>
 
-    <div>
-      <ul>
-        <li v-for="(item, index) in chartList">
-          <span></span>
-          <span>{{ name }}</span>
-          <span>{{ number }}명</span>
-        </li>
-      </ul>
-    </div>
-  </LayoutLong>
+                <li class="list__text-box">
+                    <span class="list__text-box__box"></span>
+                    <span class="list__text-box__text">{{ name }}</span>
+                    <span>{{ number }}명</span>
+                </li>
+
+                <li class="list__text-box">
+                    <span class="list__text-box__box"></span>
+                    <span class="list__text-box__text">{{ name }}</span>
+                    <span>{{ number }}명</span>
+                </li>
+            </ul>
+        </div>
+    </LayoutLong>
 </template>
 
 <script>
@@ -23,55 +34,82 @@ import LayoutLong from "../Layout/LayoutLong.vue";
 import Details from "../components/Button/Details.vue";
 import BarChart from "../components/BarChart.vue";
 export default {
-  components: { Details, BarChart, LayoutLong },
+    components: { Details, BarChart, LayoutLong },
 
-  data() {
-    return {
-      chartList: [],
-    };
-  },
-
-  methods: {
-    getData() {
-      const newArr = {
-        name: "지에스아이엘",
-        number: "20",
-      };
-
-      this.chartList = newArr;
+    data() {
+        return {
+            name: "지에스아이엘",
+            number: "10",
+        };
     },
-  },
-
-  mounted() {
-    this.getData();
-  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../scss/Font.scss";
 .layout {
-  height: 230px;
-  position: relative;
+    height: 230px;
+    position: relative;
 
-  &__chart {
-    display: flex;
+    &__chart {
+        display: flex;
 
-    width: 140px;
-    height: 140px;
+        width: 140px;
+        height: 140px;
 
-    margin-top: 24px;
-    margin-left: 24px;
-  }
+        margin-top: 24px;
+        margin-left: 24px;
+    }
 
-  &__total {
-    position: absolute;
+    &__wrap {
+        display: flex;
+        justify-content: space-between;
 
-    top: 133px;
-    left: 86px;
+        &__total {
+            position: absolute;
 
-    font-size: 30px;
-    font-weight: $font-weight-500;
-  }
+            top: 135px;
+            left: 92px;
+
+            font-size: 30px;
+            font-weight: $font-weight-500;
+        }
+
+        .list {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+
+            padding: 0;
+
+            width: 55%;
+
+            font-weight: 600;
+            font-weight: 600;
+
+            &__text-box {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+
+                &__box {
+                    display: block;
+                    content: "";
+
+                    width: 15px;
+                    height: 15px;
+                    margin-right: 8px;
+
+                    background-color: #1a4568;
+                    border-radius: 4px;
+                }
+
+                &__text {
+                    margin-right: 8px;
+                }
+            }
+        }
+    }
 }
 </style>
